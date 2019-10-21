6ea3bc822d26475f1da97522e96966e45bb743df
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 11:47:47 +0530

    adding static create method

--- balltripathlineview/src/main/java/com/anwesh/uiprojects/balltripathlineview/BallTriPathLineView.kt
+++ balltripathlineview/src/main/java/com/anwesh/uiprojects/balltripathlineview/BallTriPathLineView.kt
@@ -208,4 +208,13 @@ class BallTriPathLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : BallTriPathLineView {
+            val view : BallTriPathLineView = BallTriPathLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
