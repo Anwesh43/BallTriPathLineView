355aadc0ceff73e3812126be33f38fb4864d56c5
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 10:50:50 +0530

    adding constants and importing libraries

--- balltripathlineview/src/main/java/com/anwesh/uiprojects/balltripathlineview/BallTriPathLineView.kt
+++ balltripathlineview/src/main/java/com/anwesh/uiprojects/balltripathlineview/BallTriPathLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.balltripathlineview
 /**
  * Created by anweshmishra on 21/10/19.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val parts : Int = 4
+val scGap : Float = 0.01f
+val delay : Long = 20
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
