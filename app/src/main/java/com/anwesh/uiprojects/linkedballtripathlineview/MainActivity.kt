52a6a017f53c831917e591dda063e488ed6c775f
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 11:59:19 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedballtripathlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballtripathlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedballtripathlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.balltripathlineview.BallTriPathLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BallTriPathLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
